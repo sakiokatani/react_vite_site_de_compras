3caa648fdeee65fd7848e71cae409c4de792cb7c
Saki Okatani <saki.okatani1@gmail.com>
2023-07-02 09:51:01 -0300

    cards ordering acc screen-size

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -3,58 +3,32 @@ import React, { useState, useEffect } from "react";
 import Button from './Button.jsx'
 import productCardStyle from "../styles/ProductCard.css"
 import PropTypes from 'prop-types'
-import typewriter from '../../public/assets/typewriter.jpg'
-// import ProductData from '../product_data/products.json'
 
-function ProductCard({id, nome, descricao, preco}){
+function ProductCard({id, nome, descricao, preco, imagem}){
     
     const button={
         prop1:"Comprar"
     }
-    const [data, setData] = useState([]);
-    const [imagePath, setImagePath] = useState('');
 
-    useEffect(() => {
-      fetchData();
-    }, []);
- 
-    const BASEPATH = "/assets/FILE";
-  
-    const fetchData = async () => {
-      try {
-         const response = await fetch(ProductData);
-         const jsonData = await response.json();
-         setData(jsonData);
-         const imagePath = jsonData.imagem;
-         const updatedPath = BASEPATH.replace('FILE', imagePath);
-         setImagePath(updatedPath);
-         console.log(updatedPath);
-        
-        console.log(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-return(
-    <>
-    <div className="container-flex">
-            <div className="image-wrapper">
-                <img src={imagePath} alt={nome}></img>
-            </div>
-            <div className="bottom-wrapper">
-                <div className="description-wrapper">
-                    <p id={id}>{nome}</p>
-                    <p id={id}>{descricao}</p>
-                    <p id={id}>{preco}</p>
+    return(
+        <>
+        <div className="container-flex">
+                <div className="image-wrapper">
+                    <img class="fotoProduto" src={imagem} alt={nome}></img>
                 </div>
-                <div>
-                    <Button prop1={button.prop1} />
+                <div className="bottom-wrapper">
+                    <div className="description-wrapper">
+                        <h4 id={id}>{nome}</h4>
+                        <p id={id}>{descricao}</p>
+                        <p id={id}>{preco}</p>
+                    </div>
+                    <div>
+                        <Button prop1={button.prop1} />
+                    </div>
                 </div>
-            </div>
-    </div>
-    </>
-)}
+        </div>
+        </>
+    )}
 
 
 ProductCard.propTypes={
